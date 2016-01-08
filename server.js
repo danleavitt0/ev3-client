66fad3f3ae10633da26ab630c1eb0c3a95b1e315
Daniel <danleavitt0@gmail.com>
2016-01-08 13:47:10 -0800

    update server

--- server.js
+++ server.js
@@ -21,133 +21,131 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use('/static', express.static(__dirname + '/public'))
 
-io.on('connection', function (socket) {
-  var node = createNode()
-  app.post('/file.get/:name', function (req, res) {
-    fs.readFile(__dirname + '/files/' + req.params.name, 'utf-8', function (err, data) {
-      if (err)  return res.send('var MoveSteering = require(\'move-steering\')')
-      res.send(data)
-    })
+var node = createNode()
+app.post('/file.get/:name', function (req, res) {
+  fs.readFile(__dirname + '/files/' + req.params.name, 'utf-8', function (err, data) {
+    if (err)  return res.send('var MoveSteering = require(\'move-steering\')')
+    res.send(data)
   })
-
-  app.post('/log.get', function (req,res) {
-    var file = fs.readFileSync('log.txt', 'utf-8')
-    res.json({ok: true, data: file})
-  })
-
-  app.post('/file.save', function (req, res) {
-    fs.writeFile(
-      __dirname + '/files/' + req.body.name,
-      req.body.text,
-      function (err, data) {
-        if (err) {
-          res.json({ok: false, message: 'Failed to save data, please try again.'})
-        }
-        res.json({ok: true, message: 'Save Successful'})
-      })
-  })
-
-  app.post('/file.stop', function (req, res) {
-    node.kill('SIGINT')
-    try {
-      MoveSteering().reset()
-    } catch (e) {
-      console.warn('no motors attached')
-    }
-    node = createNode()
-    io.emit('action', {type:'FINISH_SERVER', payload: {message: 'Run stopped'}})
-    res.json({
-      ok: true,
-      message: 'Run stopped'
-    })
-  })
-
-  app.post('/file.run', function (req, res) {
-    var file = __dirname + '/files/' + req.body.fileName
-    node.stdin.write(file)
-    res.json({ok: true})
-  })
-
-  app.post('/file.getAll', function (req, res) {
-    fs.readdir(__dirname + '/files', function (err, data) {
-      if (err) {
-        console.warn(err)
-      }
-      res.send(JSON.stringify(data))
-    })
-  })
-
-  app.post('/sensor.data', function (req, res) {
-    var readPath = path.join(req.body.path, req.body.ext || 'value0')
-    fs.readFile(readPath, 'utf-8', function (err, data) {
-      if (err) {
-        res.json({
-          ok: false,
-          msg: err
-        })
-      } else {
-        res.json({
-          ok: true,
-          data: {
-            value: data.trim(),
-            port: req.body.port
-          }
-        })
-      }
-    })
-  })
-
-  app.post('/sensor.mode', function (req, res) {
-    var writePath = path.join(req.body.path, 'mode')
-    fs.writeFile(writePath, req.body.mode, function (err) {
-      if (err) {
-        res.json({ 
-          ok: false,
-          msg: err
-        })
-      } else {
-        res.json({ ok: true })
-      }
-    })
-  })
-
-  app.post('/sensors.find', function (req, res) {
-    var currentDevices = ports.reduce(function (obj, port) {
-      try {
-        var path = devices(port)
-        obj[port] = {
-          path: path,
-          type: fs.readFileSync(path + '/driver_name', 'utf-8').trim()
-        }
-      } catch (e) {
-        obj[port] = {
-          type: 'No device connected'
-        }
-      }
-      return obj
-    }, {})
-    res.json({
-      ok: true,
-      currentDevices: currentDevices
-    })
-  })
-
-  function createNode () {
-    var n = spawn('node', ['run.js'])
-    n.stdout.setEncoding('utf-8')
-    n.stderr.setEncoding('utf-8')
-    n.stdout.on('data', function (data) {
-      fs.appendFileSync('log.txt', data)
-      if (data.match(/@@@/gi))
-        io.emit('action', {type: 'FINISH_SERVER', payload: {message: 'Run finished'}})
-    })
-    n.stderr.on('data', function (data) {
-      fs.appendFileSync('log.txt', '@@@\n')
-      io.emit('action', {type: 'FINISH_SERVER', payload: {message: 'Error: check the console'}})
-    })
-    return n
-  }
 })
+
+app.post('/log.get', function (req,res) {
+  var file = fs.readFileSync('log.txt', 'utf-8')
+  res.json({ok: true, data: file})
+})
+
+app.post('/file.save', function (req, res) {
+  fs.writeFile(
+    __dirname + '/files/' + req.body.name,
+    req.body.text,
+    function (err, data) {
+      if (err) {
+        res.json({ok: false, message: 'Failed to save data, please try again.'})
+      }
+      res.json({ok: true, message: 'Save Successful'})
+    })
+})
+
+app.post('/file.stop', function (req, res) {
+  node.kill('SIGINT')
+  try {
+    MoveSteering().reset()
+  } catch (e) {
+    console.warn('no motors attached')
+  }
+  node = createNode()
+  io.emit('action', {type:'FINISH_SERVER', payload: {message: 'Run stopped'}})
+  res.json({
+    ok: true,
+    message: 'Run stopped'
+  })
+})
+
+app.post('/file.run', function (req, res) {
+  var file = __dirname + '/files/' + req.body.fileName
+  node.stdin.write(file)
+  res.json({ok: true})
+})
+
+app.post('/file.getAll', function (req, res) {
+  fs.readdir(__dirname + '/files', function (err, data) {
+    if (err) {
+      console.warn(err)
+    }
+    res.send(JSON.stringify(data))
+  })
+})
+
+app.post('/sensor.data', function (req, res) {
+  var readPath = path.join(req.body.path, req.body.ext || 'value0')
+  fs.readFile(readPath, 'utf-8', function (err, data) {
+    if (err) {
+      res.json({
+        ok: false,
+        msg: err
+      })
+    } else {
+      res.json({
+        ok: true,
+        data: {
+          value: data.trim(),
+          port: req.body.port
+        }
+      })
+    }
+  })
+})
+
+app.post('/sensor.mode', function (req, res) {
+  var writePath = path.join(req.body.path, 'mode')
+  fs.writeFile(writePath, req.body.mode, function (err) {
+    if (err) {
+      res.json({ 
+        ok: false,
+        msg: err
+      })
+    } else {
+      res.json({ ok: true })
+    }
+  })
+})
+
+app.post('/sensors.find', function (req, res) {
+  var currentDevices = ports.reduce(function (obj, port) {
+    try {
+      var path = devices(port)
+      obj[port] = {
+        path: path,
+        type: fs.readFileSync(path + '/driver_name', 'utf-8').trim()
+      }
+    } catch (e) {
+      obj[port] = {
+        type: 'No device connected'
+      }
+    }
+    return obj
+  }, {})
+  res.json({
+    ok: true,
+    currentDevices: currentDevices
+  })
+})
+
+function createNode () {
+  var n = spawn('node', ['run.js'])
+  n.stdout.setEncoding('utf-8')
+  n.stderr.setEncoding('utf-8')
+  n.stdout.on('data', function (data) {
+    fs.appendFileSync('log.txt', data)
+    if (data.match(/@@@/gi))
+      io.emit('action', {type: 'FINISH_SERVER', payload: {message: 'Run finished'}})
+  })
+  n.stderr.on('data', function (data) {
+    fs.appendFileSync('log.txt', '@@@\n')
+    io.emit('action', {type: 'FINISH_SERVER', payload: {message: 'Error: check the console'}})
+  })
+  return n
+}
 
 app.get('*', function (req, res) {
   res.sendfile(__dirname + '/public/index.html')
