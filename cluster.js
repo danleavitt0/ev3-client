02c1643602ca261a21827f9fc7d33f516477824a
Daniel <danleavitt0@gmail.com>
2016-01-19 13:50:09 -0800

    fix encoding

--- cluster.js
+++ cluster.js
@@ -10,7 +10,7 @@ for (var i = 0; i < NUM_NODES; i++) {
 var idx = 0
 exports.run = function (file) {
 	var node = nodes[idx]
-	node.stdin.write(file, {encoding: 'utf-8'})
+	node.stdin.write(file, 'utf-8')
 	nodes[idx] = createSpawn()
 	idx = (idx + 1) % NUM_NODES
 	return node
