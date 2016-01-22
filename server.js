546414eced12ba58c06c39184ebc7f361b643b61
Daniel <danleavitt0@gmail.com>
2016-01-21 19:21:26 -0800

    add spawn to server

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = express()
 var http = require('http').Server(app)
 var parsetrace = require('parsetrace')
 var cluster = require('./cluster')
+var spawn = require('child_process').spawn
 
 var ports = ['a', 'b', 'c', 'd', 1, 2, 3, 4]
 
